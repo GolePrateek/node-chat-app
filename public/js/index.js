76de547530e7234bcd0120def303f329580ba052
Prateek Gole <goleprateek@gmail.com>
2018-04-24 00:24:25 +0530

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBottom(){
+  //selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop +newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+};
+
 socket.on('connect',function(){
   console.log('Connected to server.');
 });
@@ -19,7 +35,9 @@ socket.on('newMessage',function(message){
     createdAt: timestamp
   });
 
-  jQuery('#messages').append(html);
+ jQuery('#messages').append(html);
+ scrollToBottom();
+//  document.getElementById(id).scrollIntoView();
   // var li = jQuery('<li></li>');
   // li.text(`${message.from} ${timestamp}: ${message.text}`);
   //
@@ -38,6 +56,7 @@ socket.on('newLocationMessage',function(message){
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
   // var li = jQuery('<li></li>');
   // var a = jQuery('<a target="_blank">My current location</a>');
   //
